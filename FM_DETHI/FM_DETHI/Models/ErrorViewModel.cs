2060dd65a533af2d74870128d1f2f90d0d25bf81
maphihung <phihung.3497@gmail.com>
2020-01-09 16:16:54 +0700

    Base Source
    Create Project
    --------------------
    Base Source
          .Net core 3.1
          MVC Framework
    ---------------------
    Ma Phi Hung created 9/1/2020 3:58PM

---
+++ FM_DETHI/FM_DETHI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FM_DETHI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
