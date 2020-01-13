b3ca2b14326d7ea928c2719a8e2951df4a3b1d6a
maphihung <phihung.3497@gmail.com>
2020-01-13 17:20:43 +0700

    Update source API .Net Core with MVC
    Update base source
    ----------------------------------
    Mã Phi Hùng 13/1/2020 5:20 pm

---
+++ FM_DETHI/FM_DETHI/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DETHI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Pass { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public Int16 Age { get; set; }
+        public string Gender { get; set; }
+
+        public bool Check_username(string Username)
+        {
+            if (Username == this.Username)
+                return true;
+            return false;
+        } 
+        public int create(User users)
+        {
+
+            return 0;
+        }
+    }
+}
